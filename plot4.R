7e3cc47febb42ef578d8e98e74af9ac5fef4c354
ncd10 <ncda10@gmail.com>
2015-04-07 03:06:54 +0300

    fixing eportint png code

--- plot4.R
+++ plot4.R
@@ -1,8 +1,8 @@
-##plot 2
+##plot 4
 #reading in data
-#household_power_consumption <- read.csv(
-#        "C:/Users/user/Desktop/exdata-data-household_power_consumption/household_power_consumption.txt",
-#        sep=";", na.strings="?")
+household_power_consumption <- read.csv(
+        "C:/Users/user/Desktop/exdata-data-household_power_consumption/household_power_consumption.txt",        
+        sep=";", na.strings="?")
 #keeping only the specified dates
 data<-household_power_consumption[household_power_consumption$Date=="1/2/2007"|
                                           household_power_consumption$Date=="2/2/2007", ]
@@ -15,7 +15,7 @@ data$day<-as.POSIXct(paste(data$Date, as.character(data$Time)))
 Sys.setlocale("LC_TIME", "C")
 
 #plot graph
-png(filename="plot4.png",width=480, height=480)
+png(filename="plot4.png",width=504, height=504,pointsize = 14)
 
 par(mfrow=c(2,2))
 plot(data$day, data$Global_active_power, type="l", 
@@ -30,7 +30,7 @@ par(new=F)
 lines(data$day, data$Sub_metering_3, type="l", col="blue",ylim=c(0,30),xlab="",ylab="") 
 par(new=F)
 legend('topright', c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), 
-       col=c(1,2,4),lty=1, bty="n", pt.cex =2, cex=0.5)
+       col=c(1,2,4),lty=1, bty="n", pt.cex =2, cex=0.75)
 
 plot(data$day, data$Global_reactive_power, type="l", 
      ylab= "Global reactive power" ,xlab="datetime")
